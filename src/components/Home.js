7e5df8128bb4f8196379b8a9b8e0fb6ba9165079
flpegomes <felipegomesfurtado@gmail.com>
2018-08-14 16:32:58 -0300

    teste

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = ({ navigation }) => (
     <View style={styles.homeContainer}>
         <Text style={styles.textContainer}>Home xd</Text>
         <Button 
-            title="Ir para perfil"
+            title="Ir para perfil "
             onPress= {() => navigation.navigate('Perfil')}
         />
     </View>
