03514fc63022c48f191eb566949414f0b1582839
Bruno10b <bruno.bm.cabral@gmail.com>
2018-08-16 23:19:18 -0300

    :construction: Adicionado o Maps API e geolocation

--- src/components/Mapa.js
+++ src/components/Mapa.js
@@ -1,57 +1,115 @@
 import React, { Component } from 'react';
-import { View, Button, Text, StyleSheet } from 'react-native';
-import MapboxGL from '@mapbox/react-native-mapbox-gl';
+import { View, Button, Text, StyleSheet, Dimensions  } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import CustomMapStyle from './Styles/MapStyles'
 
-MapboxGL.setAccessToken('pk.eyJ1IjoiZmxwZTciLCJhIjoiY2prdmI4ZnJvMGU4ZTNrbnh6cW81eTZwNCJ9.tZPofkfClB5nc0-18e8ksQ');
 
+let { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 0;
+const LONGITUDE = 0;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default class Mapa extends Component {
-    renderAnnotations() {
-      return (
-        <MapboxGL.PointAnnotation
-          id='rocketseat'
-          coordinate={[-46.71346979, -23.63245859]}
-        >
-          <View style={styles.annotationContainer}>
-            <View style={styles.annotationFill} />
-          </View>
-          <MapboxGL.Callout title='Rocketseat House' />
-        </MapboxGL.PointAnnotation>
-      )
-    }
-  
-    render() {
-      return (
-        <MapboxGL.MapView
-          centerCoordinate={[-46.71346979, -23.63245859]}
-          style={styles.container}
-          showUserLocation
-          styleURL={MapboxGL.StyleURL.Dark}
-          showUserLocation={true}
-        >
-        {this.renderAnnotations()}
-        </MapboxGL.MapView>
-      );
-    }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+      markerPosition: {
+        latitude: 0,
+        longitude: 0,
+      }
+    };
   }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    annotationContainer: {
-      width: 30,
-      height: 30,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: 'white',
-      borderRadius: 15,
-    },
-    annotationFill: {
-      width: 30,
-      height: 30,
-      borderRadius: 15,
-      backgroundColor: '#7159C1',
-      transform: [{ scale: 0.8 }],
-    }
-  });
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var initialRegion = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }
+        this.setState({initialPosition: initialRegion});
+        this.setState({markerPosition: initialRegion});
+      },
+    (error) => alert(error.message),
+    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
+    );
+    this.watchId = navigator.geolocation.watchPosition(
+      (position) => {
+        var lastRegion = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }
+        this.setState({initialPosition: lastRegion});
+        this.setState({markerPosition: lastRegion});
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
+    );
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchId);
+  }
+
+  render() {
+    return(
+      <MapView
+        provider={ PROVIDER_GOOGLE }
+        style={ styles.container }
+        onPress={e => console.log(e.nativeEvent)}
+        customMapStyle={ CustomMapStyle.styleMap }
+        //onUserLocationChange={e => console.log(e.nativeEvent)} preciso para pegar o location.precision
+        initialPosition={ this.state.initialPosition }>
+        <MapView.Marker
+          coordinate={ this.state.markerPosition }>
+          <View style={ styles.radius }>
+            <View style={ styles.marker } />
+          </View>
+        </MapView.Marker>
+      </MapView>
+    );
+    
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems:'center',
+  },
+  radius: {
+    height: 50,
+    width: 50,
+    borderRadius: 50 / 2,
+    overflow: 'hidden',
+    backgroundColor: 'rgba(0, 122, 255, 0.1)',
+    borderWidth: 1,
+    borderColor: 'rgba(0, 112, 255, 0.3)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  marker:{
+    height: 20,
+    width: 20,
+    borderWidth: 3,
+    borderColor: 'white',
+    borderRadius: 20 / 2,
+    overflow: 'hidden',
+    backgroundColor: '#007AFF',
+  }
+});
