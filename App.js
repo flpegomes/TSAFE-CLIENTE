3994f01c3d45c66753eccca7e1c6176708b66892
flpegomes <felipegomesfurtado@gmail.com>
2018-08-20 16:47:52 -0300

    :construction: Cadastro de usuario

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import Routes from './src/Routes';
 import Principal from './src/components/Principal';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+import firebase from 'firebase';
 
+class App extends Component {
+  componentWillMount() {
+    var config = {
+      apiKey: "AIzaSyBQWP7JTyJpq3uM5kq_msco2kb-RbJ3igU",
+      authDomain: "tsafe-f0c90.firebaseapp.com",
+      databaseURL: "https://tsafe-f0c90.firebaseio.com",
+      projectId: "tsafe-f0c90",
+      storageBucket: "tsafe-f0c90.appspot.com",
+      messagingSenderId: "791072086116"
+    };
+    firebase.initializeApp(config);
+  }
 
-export default class App extends Component {
   render() {
     return(
-      <Routes />
+      <Provider store={createStore(reducers)}> 
+        <Routes />
+      </Provider>
     );
   }
 }
+
+export default App;
