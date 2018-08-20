068d51c393458261c7f4c0ae544c6c64ccf2261a
flpegomes <felipe.furtado@techne.com.br>
2018-08-20 12:12:20 -0300

    App.js virou um componente de classe (warning tela inicial) (#3)

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Routes from './src/Routes';
 import Principal from './src/components/Principal';
 
-export default props => (
-  <Principal />
 
-);
+export default class App extends Component {
+  render() {
+    return(
+      <Routes />
+    );
+  }
+}
